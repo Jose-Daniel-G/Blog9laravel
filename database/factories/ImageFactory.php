b8479c340e61674219bd734ed866120399fc8f1a
Jose-Grijalba <jose.grijalba04@gmail.com>
2024-09-05 23:15:40 -0500

    image

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -9,7 +9,8 @@ class ImageFactory extends Factory
     public function definition(): array
     {
         return [
-            'url' => 'posts/'.$this->faker->image('public/storage/posts', 640, 480, null, false)//posts/imagen.jpg
+            'url' => fake()->imageUrl($width=400, $height=400),
+            // 'url' => 'posts/'.$this->faker->image('public/storage/posts', 640, 480, null, false)//posts/imagen.jpg
         ];
     }
 }
--- resources/views/components/card-post.blade.php
+++ resources/views/components/card-post.blade.php
@@ -1,6 +1,6 @@
 @props(['post'])
 <article class="mb-8 bg-white shadow-lg rounded-lg overflow-hidden">
-    <img class="w-full h-72 object-cover object-center" src="{{ Storage::url($post->image->url) }}" alt="">
+    <img class="w-full h-72 object-cover object-center" src="{{ url($post->image->url) }}" alt="">
     <div class="px-6 py-4">
         <h1 class="font-bold text-xl mb-2">
             <a href="{{ route('posts.show', $post) }}">{{ $post->name }}</a>
